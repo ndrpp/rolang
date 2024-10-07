9bb23d7a2a7e41dfd16e95906ac738ec708e55f8
ndrpp <popandrei230@yahoo.com>
2024-10-07 21:51:36 +0300

    Add basic repl

---
+++ repl/repl.go
@@ -0,0 +1,24 @@
+package repl
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+)
+
+const PROMPT = ">> "
+
+func Start(in io.Reader, out io.Writer) {
+	scanner := bufio.NewScanner(in)
+
+	for {
+		fmt.Fprintf(out, PROMPT)
+		scanned := scanner.Scan()
+		if !scanned {
+			return
+		}
+
+		io.WriteString(out, scanner.Text())
+		io.WriteString(out, "\n")
+	}
+}
